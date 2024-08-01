e58aaf909fe30b58fa06ed0517bf64fef1ea84be
halloanewahyudi <30697981+halloanewahyudi@users.noreply.github.com>
2024-08-01 17:17:54 +0700

    sectin three

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import Navbar from './components/elements/Navbar.vue'
 import SectionOne from './components/sections/SectionOne.vue'
 import SectionTwo from './components/sections/SectionTwo.vue'
+import SectionThree from './components/sections/SectionThree.vue'
 </script>
 <template>
   <main class="plus-jakarta-sans-font">
@@ -9,6 +10,7 @@ import SectionTwo from './components/sections/SectionTwo.vue'
 <div>
   <SectionOne />
   <SectionTwo />
+  <SectionThree />
 </div>
   </main>
 
--- src/components/sections/SectionThree.vue
+++ src/components/sections/SectionThree.vue
@@ -1,22 +1,24 @@
-<script setup >
+<script setup>
 import Summary from '../elements/Summary.vue'
 import Card from '../elements/Card.vue'
 const data = [
     {
-        title:'Loremipsum',
-        description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, est?',
-        image:'https://images.pexels.com/photos/7150650/pexels-photo-7150650.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        title: 'Loremipsum',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, est?',
+        image: 'https://images.pexels.com/photos/7150650/pexels-photo-7150650.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     },
     {
-        title:'Loremipsum lagi',
-        description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, est?',
-        image:'https://images.pexels.com/photos/1010269/pexels-photo-1010269.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        title: 'Loremipsum lagi',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, est?',
+        image: 'https://images.pexels.com/photos/1010269/pexels-photo-1010269.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     }
 ]
 </script>
 <template>
-    <div class="flex gap-9">
+    <div class="max-w-screen-lgmx-auto">
+        <div class="flex gap-9">
             <Summary />
-
+        </div>
     </div>
+
 </template>
