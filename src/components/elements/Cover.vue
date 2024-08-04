d0aaf169a49ec5cd41397f22ce61c233548ee5bc
halloanewahyudi <30697981+halloanewahyudi@users.noreply.github.com>
2024-08-04 21:48:08 +0700

    posts

--- src/components/elements/Cover.vue
+++ src/components/elements/Cover.vue
@@ -26,10 +26,9 @@ const props = defineProps({
 })
 </script>
 <template>
-    <div class="cover rounded-xl p-6  relative overflow-hidden flex flex-col justify-end bg-cover border border-primary-500" 
-    :class="mn-h-[`${props.height}px`]" 
-    :style="{backgroundImage:`url(${props.image})`, height:`${props.height}px`}">
-        <div class="absolute top-0 left-0 w-full h-full bg-gradient-to-tr from-primary-500 to-secondary-600 opacity-80"></div>
+    <div class="cover rounded-xl  relative overflow-hidden" :style="{minHeight:`${props.height}px`}">
+        <div class=" flex flex-col justify-end  p-6  border border-primary-500  bg-gradient-to-tr from-primary-500 to-secondary-600" :style="{minHeight:`${props.height}px`}" >
+        <div :class="min-h-[`${props.height}px`]"  class="absolute top-0 left-0 w-full h-full  opacity-30 hover:scale-110 duration-300 bg-cover" :style="{backgroundImage:`url(${props.image})`, minHeight:`${props.height}px`}"></div>
         <div class="text-white relative">
             <h5 class="text-2xl font-semibold mb-5">{{ props.title }}</h5>
             <p>{{ props.descripiton }}</p>
@@ -37,5 +36,6 @@ const props = defineProps({
                 <a :href="readmore_link"> {{ readmore_text }}</a>
             </div>
         </div>
+    </div>
     </div>
 </template>
